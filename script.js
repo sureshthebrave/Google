7bc9802c2dc9aab98c7d6ebf740c6a44ebfbced9
sureshthebrave <sureshpachipala9704@gmail.com>
2023-10-28 21:18:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function search() {
+    let query = document.querySelector(".query").value;
+    return (window.location = "https://www.google.com/search?q=" + query);
+  }
+
+function doodles(){
+  document.querySelector(".feel");
+  return(window.location="https://www.google.com/doodles")
+}
+
